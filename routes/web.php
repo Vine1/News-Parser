db2a16ccc765628a3b85d90ac82a462f6d3abc47
Vine <vine@mail.ru>
2020-05-03 22:20:04 +0300

    Rotues comments

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,20 @@ $router->get('/', function () {
     return "";
 });
 
-// Api to manually parse data
+/*
+ * Api to manually parse data.
+ *
+ * GET params:
+ *  from: date from which to receive news.
+ */
 $router->get('/parse', "ParseController@parse");
 
-// Api to get news
+/*
+ * Api to get news
+ *
+ * GET params:
+ *  source: news source filter.
+ *  from: news published date filter.
+ *  theme: theme keyword filter.
+ */
 $router->get('/get', "NewsController@get");
